5dcf6f7d3e42018f0be9b78d44aff1e27030475e
Jay Phelps <hello@jayphelps.com>
2015-06-01 15:57:07 -0700

    Place ember runloop join in the correct spot

--- addon/schedulers/ember-scheduler.js
+++ addon/schedulers/ember-scheduler.js
@@ -4,47 +4,47 @@ var Scheduler = Rx.Scheduler;
 var SingleAssignmentDisposable = Rx.SingleAssignmentDisposable;
 
 function scheduleNow(state, action) {
-	var scheduler = this;
-	var disposable = new SingleAssignmentDisposable();
-	Ember.run.join(this, function() {
-		scheduleEmberAction(disposable, this._queue, this._target, state, action, scheduler);
-	});
-	return disposable;
+  var scheduler = this;
+  var disposable = new SingleAssignmentDisposable();
+  scheduleEmberAction(disposable, this._queue, this._target, state, action, scheduler);
+  return disposable;
 }
 
 function scheduleRelative(state, dueTime, action) {
-	var dt = Scheduler.normalize(dueTime);
-	var disposable = new SingleAssignmentDisposable();
+  var dt = Scheduler.normalize(dueTime);
+  var disposable = new SingleAssignmentDisposable();
 
-	Ember.run.later(this, function() {
-		scheduleEmberAction(disposable, this._queue, this._target, state, action, this);
-	}, dt);
+  Ember.run.later(this, function() {
+    scheduleEmberAction(disposable, this._queue, this._target, state, action, this);
+  }, dt);
 
-	return disposable;
+  return disposable;
 }
 
 function scheduleAbsolute(state, dueTime, action) {
-	return this.scheduleWithRelativeAndState(state, dueTime - Date.now(), action);
+  return this.scheduleWithRelativeAndState(state, dueTime - Date.now(), action);
 }
 
 function scheduleEmberAction(disposable, queue, target, state, action, scheduler) {
-	Ember.run.schedule(queue, target, function() {
-		if(!disposable.isDisposed) {
-			disposable.setDisposable(action(scheduler, state));
-		}
-	});
+  Ember.run.join(this, function() {
+    Ember.run.schedule(queue, target, function() {
+      if(!disposable.isDisposed) {
+        disposable.setDisposable(action(scheduler, state));
+      }
+    });
+  });
 }
 
 /**
-	Creates an Rx Scheduler that uses a specified Ember Run Loop Queue.
-	@method emberScheduler
-	@param queue {String} the name of the ember queue to create the Rx Scheduler for
-	@param target {Ember.Object} the object to use as the context of the Ember run schedule
-	@return {Rx.Scheduler}
+  Creates an Rx Scheduler that uses a specified Ember Run Loop Queue.
+  @method emberScheduler
+  @param queue {String} the name of the ember queue to create the Rx Scheduler for
+  @param target {Ember.Object} the object to use as the context of the Ember run schedule
+  @return {Rx.Scheduler}
 */
 export default function emberScheduler(queue, target) {
-	var scheduler = new Scheduler(Date.now, scheduleNow, scheduleRelative, scheduleAbsolute);
-	scheduler._target = target;
-	scheduler._queue = queue;
-	return scheduler;
+  var scheduler = new Scheduler(Date.now, scheduleNow, scheduleRelative, scheduleAbsolute);
+  scheduler._target = target;
+  scheduler._queue = queue;
+  return scheduler;
 }
